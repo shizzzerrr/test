00e4166dd0e107566c4e47e2ef560ff9b75feaed
Ilya <Ilya@LAPTOP-UPD5BCKP>
2023-03-14 16:22:16 +0700

    Добавьте файлы проекта.

---
+++ qwerty/Program.cs
@@ -0,0 +1,34 @@
+﻿const int N = 5;
+string[] s = new string[N] {"hello", 
+                            "123",
+                            "world", 
+                            ":)",
+                            "45"};
+
+int count = 0;
+
+for (int i = 0; i < s.Length; i++)
+{
+    if (s[i].ToCharArray().Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] new_s = new string[count];
+int current_index = 0;
+
+for (int i = 0; i < s.Length; i++)
+{
+    if (s[i].ToCharArray().Length <= 3)
+    {
+        new_s[current_index] = s[i];
+        current_index++;
+    }
+}
+
+Console.WriteLine("Новый массив : ");
+for (int i = 0; i < new_s.Length; i++)
+{
+    Console.Write(new_s[i] + " ");
+}
